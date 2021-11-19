72911a0832982a6219d4050a58e350b5bfee1a64
ojmel <61952149+ojmel@users.noreply.github.com>
2021-11-19 12:04:10 -0500

    Update ProductionScript.py

--- ProductionScript.py
+++ ProductionScript.py
@@ -10,6 +10,6 @@ for line in ProteinList:
     RBDFinder.AlignmentFinder(ProteinList[i,1]+'onSARS2.aln')
     SpliceBoundary=RBDFinder.AlignmentFinder
     FastasforRun[k]=RBDExchanger.RBDExchange('SARS2.fasta',Protein+'.fasta',SpliceBoundary[0],SpliceBoundary[1])
-file=open('AlphaFold.slurm','a')
+file=open('NeoAlphaFold.slurm','a')
 file.writelines('python alpha_list.py -s= -o=/scratch/jws6pq/Notebook/Finished','cd /scratch/jws6pq/Notebook/Finished','for d in ./*/ ; do (cd "$d" && python /sfs/lustre/bahamut/scratch/jws6pq/CMfiles/pickleopener.py && mydir="$(basename $PWD)" && mv overall.txt $mydir.overall && mv plddt.txt $mydir.plddt && mv relaxed_model_1.pdb $mydir.pdb); done')
 print(file)
