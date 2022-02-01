05c49c39b35e2a1b745d3bdc3b74a82c129194fb
ojmel <61952149+ojmel@users.noreply.github.com>
2022-02-01 12:39:55 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,37 +1,37 @@
 import numpy as np
 ProteinList=[line.split()[-1] for line in open('List','r').readlines()]
-Domain='S1'
-def FoldXStability(Protein,ComparisonScore=199.12):
+DomainSetting='S1'
+def FoldXStability(Protein,Domain='RBD',ComparisonScore=199.12):
     import os
     os.system('/sfs/lustre/bahamut/scratch/jws6pq/FoldX/foldx_20221231 --command=Stability --clean-mode=1 --pdb=SARS2w'+Protein+Domain+'.pdb --pdb-dir=/sfs/lustre/bahamut/scratch/jws6pq/Notebook/PDB --output-dir=/sfs/lustre/bahamut/scratch/jws6pq/Notebook/FoldXResults/')
     FoldxScore=float(open('/sfs/lustre/bahamut/scratch/jws6pq/Notebook/FoldXResults/SARS2w'+Protein+Domain+'_0_ST.fxout','r').readlines()[0].split()[1])
     FoldXDifference=-((ComparisonScore-FoldxScore)/ComparisonScore)*100
     return FoldXDifference
-FoldXScore=list(map(FoldXStability,ProteinList))
-def PieceWise(Protein,Domain='RBD',ComparisonProtein='alphasars2.pdb'):
+FoldXScore=list(map(FoldXStability,ProteinList,Domain=DomainSetting))
+def PieceWise(Protein,CPSplice1='224',CPSplice2='424',Domain='RBD',ComparisonProtein='alphasars2.pdb'):
     import RBDFinder
     cmd.load(ComparisonProtein,object='CP')
     cmd.remove('organic')
     proteinpdb = Protein + '.pdb'
     cmd.load(proteinpdb)
     # ComparisonProtein=CP
-    cmd.select('CPNonDomain', selection='CP and not resi 1-233')
-    cmd.select('CPDomain', selection='CP and resi -424')
+    cmd.select('CPNonDomain', selection='CP and not resi '+CPSplice1+'-'+CPSplice2)
+    cmd.select('CPDomain', selection='CP and resi '+CPSplice1+'-'+CPSplice2)
     SpliceBoundaries=RBDFinder.AlignmentFinder(Protein+'onSARS2.aln', Protein)
     SpliceBoundary1=str(SpliceBoundaries[0])
     SpliceBoundary2=str(SpliceBoundaries[1])
-    NTDindex = Protein+' and resi -'+SpliceBoundary1
-    RBDindex = Protein+' and resi '+ SpliceBoundary1+'-'+SpliceBoundary2
-    NTDName = Protein +'NTD'
-    RBDName = Protein +'RBD'
-    cmd.select(NTDName, selection=NTDindex)
-    NTDRMSD=cmd.super('SARS2NTD', NTDName)[0]
-    cmd.select(RBDName, selection=RBDindex)
-    RBDRMSD = cmd.super('SARS2RBD', RBDName)[0]
-    OverallRMSD = cmd.super('SARS2', Protein)[0]
+    Domainindex = Protein+' and resi '+SpliceBoundary1+'-'+SpliceBoundary2
+    NonDomainindex = Protein+' and not resi '+ SpliceBoundary1+'-'+SpliceBoundary2
+    DomainName = Protein +'Domain'
+    NonDomainName = Protein +'NonDomain'
+    cmd.select(DomainName, selection=Domainindex)
+    cmd.select(NonDomainName, selection=NonDomainindex)
+    DomainRMSD=cmd.super('CPDomain', DomainName)[0]
+    NonDomainRMSD = cmd.super('CPNonDomain', NonDomainName)[0]
+    OverallRMSD = cmd.super('CP', Protein)[0]
     cmd.delete('all')
-    return RBDRMSD, OverallRMSD
-RMSD=list(map(PieceWise,ProteinList))
+    return DomainRMSD, OverallRMSD
+RMSD=list(map(PieceWise,ProteinList,Domain=DomainSetting))
 DomainRMSD=[x[0] for x in RMSD]
 OverallRMSD=[x[1] for x in RMSD]
 import Perresiduecomparison
@@ -51,17 +51,17 @@ EmbossScore=list(map(SequenceSimilarity,ProteinList))
 
 DataChart=np.empty((len(ProteinList)+1,7),dtype=object)
 DataChart[0,0]='Protein'
-DataChart[0,1]=Domain+'RMSD'
+DataChart[0,1]=DomainSetting+'RMSD'
 DataChart[0,2]='OverallRMSD'
 DataChart[0,3]='Emboss'
 DataChart[0,4]='DomainConfidence'
 DataChart[0,5]='OverallConfidence'
 DataChart[0,6]='FoldX'
 DataChart[1:,0]=ProteinList
-DataChart[1:,1]=RBDRMSD
+DataChart[1:,1]=DomainRMSD
 DataChart[1:,2]=OverallRMSD
 DataChart[1:,3]=EmbossScore
 DataChart[1:,4]=PercentDifference
 DataChart[1:,5]=OverallPercentDifference
 DataChart[1:,6]=FoldXScore
-np.savetxt('/sfs/lustre/bahamut/scratch/jws6pq/CMfiles/'+Domain+'ChimeraAnalysis.tsv', DataChart, fmt="%s,%s,%s,%s,%s,%s,%s", delimiter="")
+np.savetxt('/sfs/lustre/bahamut/scratch/jws6pq/CMfiles/'+DomainSetting+'ChimeraAnalysis.tsv', DataChart, fmt="%s,%s,%s,%s,%s,%s,%s", delimiter="")
