687af8f42d3d69075b10204d00c6ae5d20b29b98
ojmel <jamelsmpsn@gmail.com>
2022-09-12 13:53:23 -0400

    Before major comment and refinement overhaul

--- pickleopener.py
+++ pickleopener.py
@@ -4,17 +4,14 @@ def GeneratePDBandPlddtfromAlphafold(FolderDir,AlphafoldFolder,PDBDest_Name='NA'
     import os
     import shutil
     import numpy as np
-    os.chdir(FolderDir+AlphafoldFolder)
-    #os.path.exists(PDBDest_Name)==False and os.path.exists(PlddtDest_Name)==False
-    if os.path.exists('ranking_debug.json') and :
-        jfile=open('ranking_debug.json','r')
-        print(AlphafoldFolder)
-        data=json.load(jfile)
-        HighestRankModel=data['order'][0][0:7]
-        infile=open('result_'+HighestRankModel+'_multimer_v2_pred_0.pkl','rb')
-        dat=pickle.load(infile)
-        if PlddtDest_Name!='NA':
+    Fullpath=FolderDir+AlphafoldFolder+'/'
+    if os.path.exists(Fullpath+'ranking_debug.json'):
+        if PDBDest_Name!='NA' and os.path.exists(PDBDest_Name)==False:
+            shutil.copy(Fullpath+'ranked_0.pdb',PDBDest_Name)
+        if PlddtDest_Name!='NA' and os.path.exists(PlddtDest_Name)==False:
+            jfile = open(Fullpath+'ranking_debug.json', 'r')
+            data = json.load(jfile)
+            HighestRankModel = data['order'][0][0:7]
+            infile = open(Fullpath+'result_' + HighestRankModel + '_multimer_v2_pred_0.pkl', 'rb')
+            dat = pickle.load(infile)
             np.savetxt(PlddtDest_Name,dat['plddt'],fmt="%s",delimiter=" ")
-        if PDBDest_Name!='NA':
-            shutil.copy('ranked_0.pdb',PDBDest_Name)
-
