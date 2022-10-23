5204d36abce44ff590eadeaaa9850bb166da8010
jamel <jamelsmpsn@gmail.com>
2022-10-23 13:38:50 -0400

    Before Parellelization

---
+++ ContactMap.py
@@ -0,0 +1,73 @@
+import multiprocessing
+def calc_dist_matrix(chain_one, chain_two,DistanceCutoff):
+    from numpy import array
+    # """Returns a matrix of C-alpha distances between two chains"""
+    answer=array([1 if (calc_residue_dist(residue_one, residue_two)) <= DistanceCutoff and (col-row) >= 6 else 0 for row, residue_one in enumerate(chain_one) for col, residue_two in enumerate(chain_two)]).reshape((len(chain_one), len(chain_two)))
+    return array(answer)
+#Copied from https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/protein_contact_map/
+def calc_residue_dist(residue_one, residue_two) :
+    from numpy import sqrt
+        # """Returns the C-alpha distance between two residues"""
+    diff_vector  = residue_one["CA"].coord - residue_two["CA"].coord
+    return sqrt(sum(diff_vector * diff_vector))
+def GetResidueContactPairs(PDBnickname,PDBFilename,DistanceCutoff):
+    from Bio.PDB import PDBParser
+    from numpy import where
+    pdb_code,pdb_filename = PDBnickname,PDBFilename
+    structure = PDBParser().get_structure(pdb_code, pdb_filename)
+    model = structure[0]
+    dist_matrix = calc_dist_matrix(model["B"], model["B"],DistanceCutoff)
+    X_axis,Y_axis=list(where(dist_matrix==1)[0]),list(where(dist_matrix==1)[1])
+    ListofContactPairs=[[] for x in dist_matrix]
+    for x, y in zip(X_axis, Y_axis):
+        ListofContactPairs[x].append(y)
+    return ListofContactPairs
+def CorrectResiduePositionforAlignment(Protein,alignment):
+    ContactMap = GetResidueContactPairs(Protein, '3mer' + Protein + '.pdb', 7)
+    SeqIndexing = [ind for ind, x in enumerate(alignment) if x != '-']
+    ResiduePositionDictionary = {indx: indy for indx, indy in enumerate(SeqIndexing)}
+    UpdatedContactMap = [[ResiduePositionDictionary[y] for y in ContactMap[ind]] for ind, x in enumerate(ContactMap)]
+    return UpdatedContactMap
+def ContactOverlap(Alignmentfile,reference='6vsb_B'):
+    from numpy import empty
+    from numpy import savetxt
+    # Alignment in FASTA format. Make sure your benchmark sequence is first
+    Sequences = open(Alignmentfile, "r").read().split('>')[0:4]
+    SequenceDictionary={sequence.split('\n')[0]:sequence.split('\n')[1].strip() for sequence in Sequences if len(sequence)!=0}
+    ScoreArray=empty((len(SequenceDictionary)-1,3), dtype=object)
+
+    for key, value in SequenceDictionary.items():
+        j=0
+
+        UpdatedContactMap=CorrectResiduePositionforAlignment(key,value)
+        MapwResidue=[]
+        for x in value:
+            if x.isalpha():
+                MapwResidue.append([x]+UpdatedContactMap[j])
+                j+=1
+            else:
+                MapwResidue.append([x])
+        SequenceDictionary[key]=MapwResidue
+    ReferenceContactMap=SequenceDictionary[reference]
+    del SequenceDictionary[reference]
+    j=0
+    for key, value in SequenceDictionary.items():
+        Overlap = 0
+        for x,y in zip(ReferenceContactMap,value):
+            for w in x:
+                if w in y and w!='-':
+                    Overlap+=1
+        ScoreArray[j,0]=key
+        ScoreArray[j,1]=Overlap
+        # system('/scratch/jws6pq/EMBOSS-6.6.0/emboss/needle -sprotein -gapopen 10 -gapextend 0.5 -outfile /gpfs/gpfs0/scratch/jws6pq/Notebook/Emboss/Full' + key + '.emboss -asequence /gpfs/gpfs0/scratch/jws6pq/BridNotebook/Fastas/' + key + '.fasta -bsequence /gpfs/gpfs0/scratch/jws6pq/BridNotebook/Fastas/SARS2.fasta')
+        # EmbossScore = open('/gpfs/gpfs0/scratch/jws6pq/Notebook/Emboss/Full' + key + '.emboss', 'r').readlines()[25].split()[-1]
+        # ScoreArray[j, 2] = EmbossScore.replace('(', '').replace(')', '').replace('%', '')
+        j+=1
+    # savetxt('ContactScore.tsv', ScoreArray, fmt="%s,%s,%s", delimiter="")
+    return ScoreArray
+#Do i consider all the times where there are residues beyond SARS???????
+import concurrent.futures
+if __name__ == '__main__':
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+
+
