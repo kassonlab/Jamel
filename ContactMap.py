3f6674c70ad4aa0816620f5e8fb338b58dc7c5ad
jamel <jamelsmpsn@gmail.com>
2022-12-16 11:16:39 -0500

    After Parellelization

--- ContactMap.py
+++ ContactMap.py
@@ -1,26 +1,43 @@
-def calc_dist_matrix(chain_one, chain_two,DistanceCutoff):
-    from numpy import array
-    # """Returns a matrix of C-alpha distances between two chains"""
-    answer=array([1 if (calc_residue_dist(residue_one, residue_two)) <= DistanceCutoff and (col-row) >= 6 else 0 for row, residue_one in enumerate(chain_one) for col, residue_two in enumerate(chain_two)]).reshape((len(chain_one), len(chain_two)))
-    return array(answer)
-#Copied from https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/protein_contact_map/
+
 def calc_residue_dist(residue_one, residue_two) :
+    """Returns the C-alpha distance between two residues"""
+    # Mostly copied from https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/protein_contact_map/
     from numpy import sqrt
-        # """Returns the C-alpha distance between two residues"""
+    # Using distance formula to calculate residue distance
     diff_vector  = residue_one["CA"].coord - residue_two["CA"].coord
     return sqrt(sum(diff_vector * diff_vector))
-def GetResidueContactPairs(PDBnickname,PDBFilename,DistanceCutoff):
+def residue_dist_matrix(pdb_file,chain_identifier,make_it_binary='Yes',distance_cutoff=7):
+    """Takes a distance matrix of a protein and converts it to a binary matrix that uses 1 to signify a residue
+    contact and 0 for no contact or Returns a matrix of C-alpha distances between residues in a protein chain or b"""
+    # Mostly copied from https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/protein_contact_map/
+    from numpy import array
     from Bio.PDB import PDBParser
+    RESIDUE_NUMBER_CUTOFF=6
+    # Calling the PDB structure with the parser
+    STRUCTURE = PDBParser().get_structure(pdb_file, pdb_file)
+    # Then selecting the chain
+    PROTEIN = STRUCTURE[0][chain_identifier]
+    if make_it_binary=='Yes':
+        # Using list comprehension to create a nested list
+        # then an array(protein length by protein length) of 1 to signifying a residue contact below distance_cutoff
+        # and at least 6 residue separation or 0 for no contact
+        return array([[1 if (calc_residue_dist(res_one, res_two)) <= distance_cutoff and (row-col) >= RESIDUE_NUMBER_CUTOFF else 0 for row, res_one in enumerate(PROTEIN)] for col, res_two in enumerate(PROTEIN)])
+    else:
+        # Using list comprehension to create a list then an array of each residue distance to the rest of the chain and stacking those tuples to make a matrix
+        return array([[calc_residue_dist(x,y) for x in PROTEIN] for y in PROTEIN])
+
+
+def get_residue_contact_pairs(pdb_filename,chain_identifier):
+    """Takes a pdb structure and return a nested list where each residue index in the list has a list with the python indexes for the residues its in contact with.
+    Residue indexes that have no contacts will have an empty list"""
     from numpy import where
-    pdb_code,pdb_filename = PDBnickname,PDBFilename
-    structure = PDBParser().get_structure(pdb_code, pdb_filename)
-    model = structure[0]
-    dist_matrix = calc_dist_matrix(model["B"], model["B"],DistanceCutoff)
-    X_axis,Y_axis=list(where(dist_matrix==1)[0]),list(where(dist_matrix==1)[1])
-    ListofContactPairs=[[] for x in dist_matrix]
-    for x, y in zip(X_axis, Y_axis):
-        ListofContactPairs[x].append(y)
-    return ListofContactPairs
+    DIST_MATRIX = residue_dist_matrix(pdb_filename,chain_identifier)
+    x_axis,y_axis=list(where(DIST_MATRIX==1)[0]),list(where(DIST_MATRIX==1)[1])
+    list_of_contact_pairs=[[] for x in DIST_MATRIX]
+    for x, y in zip(x_axis, y_axis):
+        list_of_contact_pairs[x].append(y)
+    return list_of_contact_pairs
+print(get_residue_contact_pairs('6VSB_B.pdb','B'))
 def CorrectResiduePositionforAlignment(Protein,alignment):
     ContactMap = GetResidueContactPairs(Protein, '3mer' + Protein + '.pdb', 7)
     SeqIndexing = [ind for ind, x in enumerate(alignment) if x != '-']
@@ -63,10 +80,8 @@ def ContactOverlap(Alignmentfile,comparison,reference='6vsb_B'):
             if v not in x and v!='-' and len(x)>1 or v not in x and v!='-' and len(y)>1:
                 TotalContacts += -1
     # system('/scratch/jws6pq/EMBOSS-6.6.0/emboss/needle -sprotein -gapopen 10 -gapextend 0.5 -outfile /gpfs/gpfs0/scratch/jws6pq/Notebook/Emboss/Full' + key + '.emboss -asequence /gpfs/gpfs0/scratch/jws6pq/BridNotebook/Fastas/' + key + '.fasta -bsequence /gpfs/gpfs0/scratch/jws6pq/BridNotebook/Fastas/SARS2.fasta')
-    # EmbossScore = open('/gpfs/gpfs0/scratch/jws6pq/Notebook/Emboss/Full' + comparison + '.emboss', 'r').readlines()[25].split()[-1]
-    # return comparison,Overlap,EmbossScore.replace('(','').replace(')','').replace('%','')
+    EmbossScore = open('/gpfs/gpfs0/scratch/jws6pq/Notebook/Emboss/Full' + comparison + '.emboss', 'r').readlines()[25].split()[-1]
+    return comparison,TotalContacts,EmbossScore.replace('(','').replace(')','').replace('%','')
 #Do i consider all the times where there are residues beyond SARS???????
-ContactOverlap('SARS2wEverythingstable.aln','WhiteHKU16')
-
-
-
+#
+#
