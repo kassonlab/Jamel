2eb47b72061bb53b76cf24f603b90ce07682bfbd
jamel <jamelsmpsn@gmail.com>
2023-03-09 12:38:42 -0500

    json reconfigure

--- CrossSpeciesChimeras.py
+++ CrossSpeciesChimeras.py
@@ -2,57 +2,167 @@ import ChimeraGenerator
 from sys import argv
 from json import load
 import AccessiontoAlignment
-argument_json=argv[1]
-protein_info=argv[2]
+from numpy import savetxt, empty
+import Analysis
+from pathlib import Path
 
+argument_json = argv[1]
+protein_info = argv[2]
+# ADD Filesort and alphafold submission and gromacs simulation
 
 with open(argument_json, 'rb') as jfile:
     argument_dict = load(jfile)["arguments"]
+    fasta_arguments = argument_dict['fasta_arguments']
+    operation_toggles = argument_dict['operation_toggles']
+    prefix_protein = argument_dict['Which protein is first, constant or variant?']
+    naming_arguments = argument_dict['naming_arguments']
+    analysis_arguments = argument_dict['analysis_arguments']
 with open(protein_info, 'r') as info_list:
     info_list = info_list.readlines()
     accession_number = [x.split()[0] for x in info_list]
-    comparison_proteins = [x.split()[1] for x in info_list]
-constant_sequence_of_interest=argument_dict['constant_sequence_of_interest']
-variant_sequence_of_interest=argument_dict['variant_sequence_of_interest']
+    protein_names = [x.split()[1] for x in info_list]
+constant_sequence_of_interest = fasta_arguments['constant_sequence_of_interest']
+variant_sequence_of_interest = fasta_arguments['variant_sequence_of_interest']
 with open(constant_sequence_of_interest, 'r') as fasta:
     constant_sequence_of_interest = ''.join([x for x in fasta if x[0] != '>' if x != '']).strip().replace('\n', '')
 with open(variant_sequence_of_interest, 'r') as fasta:
     variant_sequence_of_interest = ''.join([x for x in fasta if x[0] != '>' if x != '']).strip().replace('\n', '')
 
-constant_fasta=argument_dict['constant_protein'][1]
-variant_fasta=argument_dict['variant_protein'][1]
-character_to_replace=argument_dict['character_to_replace']
-constant_fasta_identifier=argument_dict['constant_fasta_identifier']
-variant_fasta_identifier=[argument_dict['variant_fasta_identifier'] for name in comparison_proteins]
-email=[argument_dict['email_for_accession'] for name in comparison_proteins]
-subunits=[argument_dict['number_of_subunits'] for name in comparison_proteins]
-msa=[argument_dict['msa_file_name'] for name in comparison_proteins]
-constant_sequence_of_interest=[constant_sequence_of_interest for name in comparison_proteins]
-variant_sequence_of_interest=[variant_sequence_of_interest for name in comparison_proteins]
-monomer_fastas=[argument_dict['monomer_fastas'][1].replace(character_to_replace,name) for name in comparison_proteins]
-multimer_fastas=[argument_dict['multimer_fastas'][1].replace(character_to_replace,name) for name in comparison_proteins]
-chimera_fastas=[argument_dict['chimera_fastas'].replace(character_to_replace,name) for name in comparison_proteins]
+constant_fasta = fasta_arguments['constant_fasta']
+variant_fasta = fasta_arguments['variant_fasta']
+placeholder = naming_arguments['placeholder']
+constant_fasta_identifier = fasta_arguments['constant_fasta_identifier']
+variant_fasta_identifier = [fasta_arguments['variant_fasta_identifier'] for name in protein_names]
+msa = [fasta_arguments['msa_file_name'] for name in protein_names]
+constant_sequence_of_interest = [constant_sequence_of_interest for name in protein_names]
+variant_sequence_of_interest = [variant_sequence_of_interest for name in protein_names]
+number_of_subunits = [fasta_arguments['number_of_subunits'] for x in protein_names]
 
-if subunits[0]==1 and argument_dict['monomer_fastas'][0]=='':
-    list(map(AccessiontoAlignment.accession_to_fasta, monomer_fastas, accession_number, email, subunits))
-elif subunits[0]>1 and argument_dict['multimer_fastas'][0]=='':
-    list(map(AccessiontoAlignment.accession_to_fasta,monomer_fastas, accession_number, email,subunits, multimer_fastas))
-
-if argument_dict['muscle_command_for_msa'][0]=='':
-    AccessiontoAlignment.multiple_sequence_alignment(monomer_fastas, argument_dict['msa_fasta'], msa[0],
-                                                     variant_fasta, argument_dict['muscle_command_for_msa'][1])
-variant_sequences=[x[0] for x in map(AccessiontoAlignment.alignment_finder, msa, variant_sequence_of_interest, comparison_proteins,
-                                     variant_fasta_identifier)]
-if argument_dict['constant_protein'][0]=='#' and argument_dict['variant_protein'][0]=='':
-    chimera_sequences=[constant_sequence_of_interest[0] + variants for variants in variant_sequences]
-elif argument_dict['constant_protein'][0]=='' and argument_dict['variant_protein'][0]=='#':
-    chimera_sequences=[variants + constant_sequence_of_interest[0] for variants in variant_sequences]
-list(map(ChimeraGenerator.fasta_creation,chimera_fastas,chimera_sequences,subunits))
-if argument_dict['fasta_file_list_name'][0] == '':
-    if subunits == 1:
-        fasta_list = monomer_fastas + chimera_fastas
+if operation_toggles['run_fasta_operation?'] == '#':
+    email = [fasta_arguments['email_for_accession'] for name in protein_names]
+    monomer_fastas = [naming_arguments['fasta_directory'] + naming_arguments['monomer_naming_convention'].replace(
+        placeholder, name) + naming_arguments['fasta_file_extension'] for name in protein_names]
+    chimera_fastas = [naming_arguments['fasta_directory'] + naming_arguments['chimera_naming_convention'].replace(
+        placeholder, name) + naming_arguments['fasta_file_extension'] for name in protein_names]
+    fasta_toggles = fasta_arguments['fasta_toggles']
+    if number_of_subunits[0] == 1:
+        list(map(AccessiontoAlignment.accession_to_fasta, monomer_fastas, accession_number, email, number_of_subunits))
     else:
-        fasta_list = multimer_fastas + chimera_fastas
-    with open(argument_dict['fasta_file_list_name'][1], 'w') as fasta_list_file:
-        for fasta in fasta_list:
-            fasta_list_file.write(f'{fasta}\n')
+        multimer_fastas = [naming_arguments['fasta_directory'] + naming_arguments['multimer_naming_convention'].replace(
+            placeholder, name) + naming_arguments['fasta_file_extension'] for name in protein_names]
+        list(map(AccessiontoAlignment.accession_to_fasta, monomer_fastas, accession_number, email, number_of_subunits,
+                 multimer_fastas))
+
+    if fasta_toggles['Create an alignment?'] == '#':
+        AccessiontoAlignment.multiple_sequence_alignment(monomer_fastas, fasta_arguments['msa_fasta'], msa[0],
+                                                         variant_fasta, fasta_arguments['muscle_command_for_msa'])
+    variant_sequences = [x[0] for x in
+                         map(AccessiontoAlignment.alignment_finder, msa, variant_sequence_of_interest, protein_names,
+                             variant_fasta_identifier)]
+    if prefix_protein == 'constant':
+        chimera_sequences = [constant_sequence_of_interest[0] + variants for variants in variant_sequences]
+    elif prefix_protein == 'variant':
+        chimera_sequences = [variants + constant_sequence_of_interest[0] for variants in variant_sequences]
+    list(map(ChimeraGenerator.fasta_creation, chimera_fastas, chimera_sequences, number_of_subunits))
+    if fasta_toggles['Make a list of created fasta files?'] == '#':
+        if number_of_subunits == 1:
+            fasta_list = monomer_fastas + chimera_fastas + fasta_arguments['constant_fasta_for_alphafold']
+        else:
+            fasta_list = multimer_fastas + chimera_fastas + fasta_arguments['constant_fasta_for_alphafold']
+        with open(fasta_arguments['fasta_list_file_name'], 'w') as fasta_list_file:
+            for fasta in fasta_list:
+                fasta_list_file.write(f'{fasta}\n')
+if operation_toggles['run_analysis_operation'] == '#':
+    plddt_directory = naming_arguments['plddt_directory']
+    analysis_toggles = analysis_arguments['analysis_toggles']
+    plddt_extension = naming_arguments['plddt_file_extension']
+    naming_convention = 'monomer_naming_convention' if number_of_subunits[
+                                                           0] == 1 else naming_convention = 'multimer_naming_convention'
+    native_plddts = [plddt_directory + naming_arguments[naming_convention].replace(
+        placeholder, protein) + plddt_extension for protein in
+                     protein_names]
+    chimera_plddt = [plddt_directory + naming_arguments['chimera_naming_convention'].replace(
+        placeholder, protein) + plddt_extension for protein in
+                     protein_names]
+    plddt_files = native_plddts + chimera_plddt
+    constant_plddt = [analysis_arguments['constant_plddt'] for x in chimera_plddt]
+    emboss_files = [naming_arguments['emboss_names'].replace(placeholder, protein) for protein in
+                    protein_names]
+    pdbs = ['NA' for x in plddt_files]
+    column_names = analysis_arguments['column_names']
+    average_relative_stability = []
+    chimera_boundary = [(0, len(constant_sequence_of_interest[0])),
+                        (len(constant_sequence_of_interest[0]), None)]
+    if prefix_protein == 'variant': chimera_boundary = chimera_boundary[::-1]
+    alphafold_folders = [analysis_arguments['alphafold_outputs_directory'] + Path(file).stem + '/' for file in
+                         plddt_files]
+    if analysis_toggles['make_pdbs?'] == '#':
+        pdbs = [naming_arguments['pdb_directory'] + Path(file).stem + naming_arguments['pdb_extension'] for file in
+                plddt_files]
+    if analysis_toggles['make_plddts?'] == '#':
+        list(map(Analysis.generate_alphafold_files, alphafold_folders, plddt_files, pdbs))
+        Analysis.generate_alphafold_files(
+            analysis_arguments['alphafold_outputs_directory'] + analysis_arguments[
+                'constant_alphafold_folder_name'] + '/',
+            constant_plddt[0])
+        if number_of_subunits[0] > 1:
+            averaged_native_plddt = [
+                plddt_directory + naming_arguments['averaged_multimer_naming_convention'].replace(placeholder,
+                                                                                                  protein) + plddt_extension
+                for protein
+                in protein_names]
+            averaged_chimera_plddt = [
+                plddt_directory + naming_arguments['averaged_chimera_naming_convention'].replace(placeholder,
+                                                                                                 protein) + plddt_extension
+                for protein
+                in protein_names]
+            list(map(Analysis.averaging_multimer_plddt, native_plddts, averaged_native_plddt, number_of_subunits))
+            list(map(Analysis.averaging_multimer_plddt, chimera_plddt, averaged_chimera_plddt, number_of_subunits))
+            Analysis.averaging_multimer_plddt(analysis_arguments['constant_plddt'],
+                                              analysis_arguments['averaged_constant'],
+                                              number_of_subunits[0])
+            averaged_constant_plddt = [analysis_arguments['averaged_constant'] for x in averaged_chimera_plddt]
+    constant_boundary = \
+    AccessiontoAlignment.alignment_finder(constant_fasta, constant_sequence_of_interest[0], constant_fasta_identifier,
+                                          constant_fasta_identifier)[
+        1]
+    if analysis_toggles['make_emboss_files?'] == '#':
+        variant_splice_info = list(
+            map(AccessiontoAlignment.alignment_finder, msa, variant_sequence_of_interest, protein_names,
+                variant_fasta_identifier, [analysis_arguments['emboss_command'] for x in emboss_files],
+                emboss_files))
+    else:
+        variant_splice_info = list(
+            map(AccessiontoAlignment.alignment_finder, msa, variant_sequence_of_interest, protein_names,
+                variant_fasta_identifier))
+    variant_splice_info = [x[1] for x in variant_splice_info]
+    if number_of_subunits == 1:
+        for index, boundary in enumerate(variant_splice_info):
+            constant_stability = Analysis.relative_stability(constant_plddt[index], constant_boundary,
+                                                             chimera_plddt[index], chimera_boundary[0])
+            variant_stability = Analysis.relative_stability(native_plddts[index], variant_splice_info[index],
+                                                            chimera_plddt[index], chimera_boundary[1])
+            average_relative_stability.append(
+                (constant_stability[0] + variant_stability[0]) / (constant_stability[1] + variant_stability[1]))
+    else:
+        for index, boundary in enumerate(variant_splice_info):
+            constant_stability = Analysis.relative_stability(averaged_constant_plddt[index], constant_boundary,
+                                                             averaged_chimera_plddt[index],
+                                                             chimera_boundary[0])
+            variant_stability = Analysis.relative_stability(averaged_native_plddt[index], variant_splice_info[index],
+                                                            averaged_chimera_plddt[index], chimera_boundary[1])
+            average_relative_stability.append(
+                (constant_stability[0] + variant_stability[0]) / (constant_stability[1] + variant_stability[1]))
+
+    if column_names['similarity'][0] == '#':
+        similarity = list(map(Analysis.get_sequence_similarity, emboss_files))
+    if column_names['overall_native_stability'][0] == '#':
+        overall_native_stability = list(map(Analysis.overall_confidence, native_plddts))
+    if column_names['overall_chimera_stability'][0] == '#':
+        overall_chimera_stability = list(map(Analysis.overall_confidence, chimera_plddt))
+    column_names = {key: value[1] for (key, value) in analysis_arguments['column_names'].items() if value[0] == '#'}
+    data_array = empty((len(average_relative_stability) + 1, len(column_names)), dtype=object)
+    for column_count, (corresponding_data, column_name) in enumerate(column_names.items()):
+        exec(f'data_array[0, column_count], data_array[1:, column_count] = column_name, corresponding_data')
+    savetxt(analysis_arguments['analysis_output_csv'], data_array, fmt=','.join('%s' for x in column_names),
+            delimiter=",")
--- ShiftedSplice.py
+++ ShiftedSplice.py
@@ -3,14 +3,14 @@ from json import load
 from ChimeraGenerator import fasta_creation
 import Analysis
 from AccessiontoAlignment import alignment_finder
-from numpy import empty,savetxt
+from numpy import empty, savetxt
 
 argument_json = argv[1]
 with open(argument_json, 'rb') as jfile:
     argument_dict = load(jfile)["arguments"]
     operation_toggles = argument_dict['operation_toggles']
-    fasta_arguments=argument_dict['fasta_arguments']
-    analysis_arguments=argument_dict['analysis_arguments']
+    fasta_arguments = argument_dict['fasta_arguments']
+    analysis_arguments = argument_dict['analysis_arguments']
 with open(fasta_arguments['alignment_file_name'], "r") as alignment:
     alignment = alignment.read().split('>')
     # Splitting up the sequences names and sequences into a dictionary
@@ -19,35 +19,36 @@ with open(fasta_arguments['alignment_file_name'], "r") as alignment:
 scanner_length = fasta_arguments['scanner_length']
 if scanner_length < 15:
     raise RuntimeError("scanner_length must be at 15 units")
-naming_settings=argument_dict['naming_arguments']
-naming_convention=naming_settings['naming_convention']
-fasta_toggles=fasta_arguments['fasta_toggles']
+naming_settings = argument_dict['naming_arguments']
+naming_convention = naming_settings['naming_convention']
+fasta_toggles = fasta_arguments['fasta_toggles']
 # define number of movements
-ref_identifier=fasta_arguments['reference_identifier']
-par_identifier=fasta_arguments['partner_identifier']
+ref_identifier = fasta_arguments['reference_identifier']
+par_identifier = fasta_arguments['partner_identifier']
 scanner_start = fasta_arguments['scanner_start']
-scanner_end = scanner_start +scanner_length
+scanner_end = scanner_start + scanner_length
 scanner_movement_size = fasta_arguments['scanner_movement_size']
 reference_sequence = sequence_dictionary[ref_identifier]
 num_of_movements = fasta_arguments['num_of_movements'][1]
 no_gap_reference_sequence = ''.join(x for ind, x in enumerate(reference_sequence) if x.isalpha())
-reference_length=len(no_gap_reference_sequence)
+reference_length = len(no_gap_reference_sequence)
 number_of_subunits = fasta_arguments['number_of_subunits']
-filename_stems=[]
-reference_boundaries=[]
-averaged_file_stems=[]
-reference_cuts=[]
-partner_cuts=[]
-partner_boundaries=[] # you need to use aceession alignment
+filename_stems = []
+reference_boundaries = []
+averaged_file_stems = []
+reference_cuts = []
+partner_cuts = []
+partner_boundaries = []  # you need to use aceession alignment
 if fasta_toggles['Manually control number of scanner movements?'] == '':
     while scanner_end < reference_length:
         file_stem = naming_convention.replace(fasta_arguments['reference_placeholder'],
-                                                                  ref_identifier).replace(
+                                              ref_identifier).replace(
             fasta_arguments['partner_placeholder'], par_identifier).replace(
             fasta_arguments['boundary_placeholder'], f'{scanner_start + 1}to{scanner_end}')
         filename_stems.append(file_stem)
-        averaged_stem = naming_settings['averaged_plddt_naming_convention'].replace(fasta_arguments['reference_placeholder'],
-                                                                  ref_identifier).replace(
+        averaged_stem = naming_settings['averaged_plddt_naming_convention'].replace(
+            fasta_arguments['reference_placeholder'],
+            ref_identifier).replace(
             fasta_arguments['partner_placeholder'], par_identifier).replace(
             fasta_arguments['boundary_placeholder'], f'{scanner_start + 1}to{scanner_end}')
         spliced_out = no_gap_reference_sequence[scanner_start:scanner_end]
@@ -64,7 +65,7 @@ else:
     count = 0
     while count <= num_of_movements:
         file_stem = naming_convention.replace(naming_settings['reference_placeholder'],
-                                                                  ref_identifier).replace(
+                                              ref_identifier).replace(
             naming_settings['partner_placeholder'], par_identifier).replace(
             naming_settings['boundary_placeholder'], f'{scanner_start + 1}to{scanner_end}')
         filename_stems.append(file_stem)
@@ -86,47 +87,51 @@ else:
         count += 1
 # Matching python indexing for the indexing from the alignment
 # with some amount of '-' and indexing in the regular sequence
-if operation_toggles['run_fasta_operation?']=='#':
+if operation_toggles['run_fasta_operation?'] == '#':
     # Creating a regular sequence without '-'
     # Boundaries are given in python index
-    fasta_list=[]
-    for sequence_out,stem,sequence_in in zip(reference_cuts, filename_stems, partner_cuts):
-        spliced_sequence = no_gap_reference_sequence.replace(sequence_out,sequence_in)
+    fasta_list = []
+    for sequence_out, stem, sequence_in in zip(reference_cuts, filename_stems, partner_cuts):
+        spliced_sequence = no_gap_reference_sequence.replace(sequence_out, sequence_in)
         fasta_name = f'{naming_settings["fasta_directory"]}{stem}{naming_settings["fasta_file_extension"]}'
         fasta_list.append(fasta_name)
         fasta_creation(fasta_name, spliced_sequence, number_of_subunits)
-    if fasta_toggles['Make a list of created fasta files?']=='#':
+    if fasta_toggles['Make a list of created fasta files?'] == '#':
+        fasta_list.append()
         with open(fasta_arguments['fasta_file_list_name'], 'w') as list_file:
             for fasta in fasta_list:
                 list_file.write(f'{fasta}\n')
 
 # Analysis Portion
-if operation_toggles['run_analysis_operation?']=='#':
-    analysis_toggles=analysis_arguments['analysis_toggles']
-    plddt_files=[f'{naming_settings["plddt_directory"]}{stem}{"plddt_file_extension"}' for stem in filename_stems]
-    if analysis_toggles['make_plddts?']=='#':
+if operation_toggles['run_analysis_operation?'] == '#':
+    analysis_toggles = analysis_arguments['analysis_toggles']
+    plddt_files = [f'{naming_settings["plddt_directory"]}{stem}{"plddt_file_extension"}' for stem in filename_stems]
+    if analysis_toggles['make_plddts?'] == '#':
         alphafold_folders = [f"{argument_dict['alphafold_outputs_directory']}{stem}/" for stem in
                              filename_stems]
         list(map(Analysis.generate_alphafold_files, alphafold_folders, plddt_files))
-        reference_plddt= [f"{naming_settings['plddt_directory']}" \
-                         f"{ref_identifier}{naming_settings['plddt_file_extension']}" for x in filename_stems]
-        partner_plddt=[f"{naming_settings['plddt_directory']}{par_identifier}" \
-                      f"{naming_settings['plddt_file_extension']}" for x in filename_stems]
+        reference_plddt = [f"{naming_settings['plddt_directory']}" \
+                           f"{ref_identifier}{naming_settings['plddt_file_extension']}" for x in filename_stems]
+        partner_plddt = [f"{naming_settings['plddt_directory']}{par_identifier}" \
+                         f"{naming_settings['plddt_file_extension']}" for x in filename_stems]
         Analysis.generate_alphafold_files(f"{argument_dict['alphafold_outputs_directory']}"
-                                          f"{ref_identifier}",reference_plddt[0])
+                                          f"{ref_identifier}", reference_plddt[0])
         Analysis.generate_alphafold_files(f"{argument_dict['alphafold_outputs_directory']}{par_identifier}",
                                           partner_plddt[0])
 
-
         if number_of_subunits[0] > 1:
-            averaged_plddt=[f'{naming_settings["plddt_directory"]}{stem}{"plddt_file_extension"}' for stem in averaged_file_stems]
+            averaged_plddt = [f'{naming_settings["plddt_directory"]}{stem}{"plddt_file_extension"}' for stem in
+                              averaged_file_stems]
             list(map(Analysis.averaging_multimer_plddt, plddt_files, averaged_plddt, number_of_subunits))
-            averaged_reference=[f"{naming_settings['plddt_directory']}" \
-                                          f"{naming_settings['averaged_reference_plddt'].replace(fasta_arguments['reference_placeholder'],ref_identifier)}{naming_settings['plddt_file_extension']}" for x in filename_stems]
-            averaged_partner=[f"{naming_settings['plddt_directory']}" \
-                                          f"{naming_settings['averaged_partner_plddt'].replace(fasta_arguments['partner_placeholder'],ref_identifier)}{naming_settings['plddt_file_extension']}" for x in filename_stems]
+            averaged_reference = [f"{naming_settings['plddt_directory']}" \
+                                  f"{naming_settings['averaged_reference_plddt'].replace(fasta_arguments['reference_placeholder'], ref_identifier)}{naming_settings['plddt_file_extension']}"
+                                  for x in filename_stems]
+            averaged_partner = [f"{naming_settings['plddt_directory']}" \
+                                f"{naming_settings['averaged_partner_plddt'].replace(fasta_arguments['partner_placeholder'], ref_identifier)}{naming_settings['plddt_file_extension']}"
+                                for x in filename_stems]
             Analysis.averaging_multimer_plddt(f"{naming_settings['plddt_directory']}"
-                                          f"{ref_identifier}{naming_settings['plddt_file_extension']}", averaged_reference[0],
+                                              f"{ref_identifier}{naming_settings['plddt_file_extension']}",
+                                              averaged_reference[0],
                                               number_of_subunits[0])
             Analysis.averaging_multimer_plddt(f"{naming_settings['plddt_directory']}"
                                               f"{par_identifier}{naming_settings['plddt_file_extension']}",
@@ -145,11 +150,11 @@ if operation_toggles['run_analysis_operation?']=='#':
                                                                  reference_plddt, reference_cuts,
                                                                  fasta_arguments['alignment_file_name'],
                                                                  ref_identifier)))
-    overall_chimera_stability = [Analysis.overall_confidence(plddt_files)]*len(relative_stability)
-    column_names={key:value[1] for (key,value) in analysis_arguments['column_names'].items() if value[0]=='#'}
+    overall_chimera_stability = [Analysis.overall_confidence(plddt_files)] * len(relative_stability)
+    column_names = {key: value[1] for (key, value) in analysis_arguments['column_names'].items() if value[0] == '#'}
     exec(f'')
     data_array = empty((len(relative_stability) + 1, len(column_names)), dtype=object)
-    for column_count,(corresponding_data,column_name) in enumerate(column_names.items()):
+    for column_count, (corresponding_data, column_name) in enumerate(column_names.items()):
         exec(f'data_array[0, column_count], data_array[1:, column_count] = column_name, corresponding_data')
 
     savetxt(argument_dict['analysis_output_csv'], data_array, fmt=','.join('%s' for x in column_names), delimiter=",")
